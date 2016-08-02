4cea5c7bc6cfa9a5c027efa106869bba3d128a05
Mark Harper <markharper20@gmail.com>
2016-08-02 10:00:49 -0400

    basic scales

--- app/charts/stackedBar.js
+++ app/charts/stackedBar.js
@@ -2,10 +2,32 @@ import * as d3 from 'd3';
 
 // stacked bar chart creator
 export function stackedBar (el, data, options) {
-    var svg = d3.select(el),
+    let svg = d3.select(el),
     margin = options.margin,
     width = options.width - margin.left - margin.right,
-    height = options.height - margin.top - margin.bottom;
+    height = options.height - margin.top - margin.bottom,
+    g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    // setup svg height and width
+    svg
+        .attr('height', options.height)
+        .attr('width', options.width);
+
+    let xScale = d3.scaleLinear()
+        .rangeRound([0, options.width]);
+
+    let yScale = d3.scaleBand()
+        .rangeRound([options.height, 0]);
+
+    let color = d3.scaleOrdinal()
+        .range(['#9AC0DA', '#F99990']);
+        
+    // set the yScale to focus on the desired data attribute
+    yScale.domain(data.map(function (d) {
+        return d.name;
+    }));
+
+    color.domain();
 
     console.log(data);
 }
--- app/index.js
+++ app/index.js
@@ -2,12 +2,51 @@ import 'main.css'
 import {stackedBar} from './charts/stackedBar.js'
 import data from './census.csv'
 
-let options = {
+let options1 = {
     margin: { top: 20, 
         right: 60, 
         bottom: 30, 
         left: 40
-    }
+    },
+    width: 856,
+    height: 426,
+    attr: 'education_level',
+    a: 'over_50k',
+    b: 'under_50k'
 }
 
-stackedBar('.edu-bar', data, options);
+// filter data for education level
+let filteredForEdu = filterForAttr(data, options1); 
+
+// arrange data in order for education level
+let eduData = [];
+eduData.push(filteredForEdu['Did not complete high school']);
+eduData.push(filteredForEdu['High school']);
+eduData.push(filteredForEdu['Some college']);
+eduData.push(filteredForEdu['Associates']);
+eduData.push(filteredForEdu['Bachelors']);
+eduData.push(filteredForEdu['Masters']);
+eduData.push(filteredForEdu['Doctorate']);
+eduData.push(filteredForEdu['Professional school']);
+
+stackedBar('.edu-bar', eduData, options1);
+
+// filter data for attribute
+function filterForAttr (data, options) {
+    let obj = {};
+    data.forEach(function(el, i, arr) {
+        if (obj[el[options.attr]]) {
+            if (el[options.a] === '1') {
+                obj[el[options.attr]][options.a]++;
+            } else {
+                obj[el[options.attr]][options.b]++;
+            }
+        } else {
+            obj[el[options.attr]] = {};
+            obj[el[options.attr]].name = el[options.attr]
+            obj[el[options.attr]][options.a] = el[options.a] === '1' ? 1 : 0;
+            obj[el[options.attr]][options.b] = el[options.a] === '1' ? 1 : 0;
+        }
+    });
+    return obj;
+}
